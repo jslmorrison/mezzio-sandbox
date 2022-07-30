d0a6e40a5b516f3a34b8cb4023480c4e17888e42
John Morrison <jslmorrison@gmail.com>
2022-07-30 10:45:20 +0100

    Add new api route

--- config/routes.php
+++ config/routes.php
@@ -40,4 +40,5 @@ use Psr\Container\ContainerInterface;
 return static function (Application $app, MiddlewareFactory $factory, ContainerInterface $container): void {
     $app->get('/', App\Handler\HomePageHandler::class, 'home');
     $app->get('/api/ping', Api\Handler\PingHandler::class, 'api.ping');
+    $app->get('/api/users', Api\Handler\UsersHandler::class, 'api.users');
 };
